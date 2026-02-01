2ba5999a894b672baa58317293deb11008b4e888
Aurelijus Stanaitis <aurelijussta@stat.gov.lt>
2026-02-02 00:29:30 +0200

    Enhance entity population handling in findBySlug method
    - Updated the population parameter to default to '*' if not provided in the query.
    - Added filtering for localizations to include only specific fields (documentId, slug, locale) if they exist.

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -18,7 +18,7 @@ module.exports = ({ strapi }) => ({
         filters: { slug },
         locale: locale || undefined, // If not provided, Strapi defaults (usually to all or default locale depending on config)
         status: status,
-        populate: ctx.query.populate, // Pass populate param from query
+        populate: ctx.query.populate || '*', // Pass populate param from query or default to '*'
       });
 
       if (!entity) {
@@ -26,6 +26,15 @@ module.exports = ({ strapi }) => ({
       }
 
       const sanitizedEntity = await strapi.contentAPI.sanitize.output(entity, strapi.getModel(uid));
+
+      // Filter localizations to include only specific fields if they exist
+      if (sanitizedEntity.localizations && Array.isArray(sanitizedEntity.localizations)) {
+        sanitizedEntity.localizations = sanitizedEntity.localizations.map(loc => ({
+          documentId: loc.documentId,
+          slug: loc.slug,
+          locale: loc.locale
+        }));
+      }
 
       ctx.body = { data: sanitizedEntity };
     } catch (error) {
